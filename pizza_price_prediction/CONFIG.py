d523aaa45a2be463f44d2db62de3e08c2ed4dc67
PranitJadhav11 <pranitj337@gmail.com>
2023-03-14 01:46:18 +0530

    Commiting all files of pizza price prediction

---
+++ pizza_price_prediction/CONFIG.py
@@ -0,0 +1,3 @@
+HOST_NUMBER = "0.0.0.0"
+PORT_NUMBER = 8000
+MODEL_PATH = r'pizza_price_prediction\artifacts\Pizza_Price_Prediction_Model.pkl'
---
+++ pizza_price_prediction/main.py
@@ -0,0 +1,29 @@
+from flask import Flask , render_template, request
+import numpy as np
+import pickle
+import CONFIG
+
+
+app = Flask(__name__)
+
+
+
+@app.route("/",methods = ["GET" , "POST"])
+def price():
+    prediction = 0
+    if request.method == "POST":
+        Extra_cheese = int(request.form.get("Extra_cheese"))
+        Extra_mushroom = int(request.form.get("Extra_mushroom"))
+        Size_by_Inch = int(request.form.get("Size_by_Inch"))
+        Extra_spicy = int(request.form.get("Extra_spicy"))
+        print(Extra_cheese , Extra_mushroom , Size_by_Inch , Extra_spicy  )
+        print(type(Size_by_Inch))
+        model = pickle.load(open(CONFIG.MODEL_PATH,"rb"))
+        prediction = model.predict([[Extra_cheese , Extra_mushroom , Size_by_Inch , Extra_spicy  ]]).astype(int)
+        print(prediction.astype(int))
+    return render_template("index.html" , Predict = prediction)
+
+
+if __name__ == "__main__":
+    app.run(host=CONFIG.HOST_NUMBER,port=CONFIG.PORT_NUMBER,debug=True)
+
