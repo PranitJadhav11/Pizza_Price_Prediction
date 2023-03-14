5c2d8716aad91946c254b2f9e54db6371b786471
Shridhar Shende <88563949+shri50@users.noreply.github.com>
2023-03-14 11:53:31 +0530

    path changed

--- pizza_price_prediction/CONFIG.py
+++ pizza_price_prediction/CONFIG.py
@@ -1,3 +1,3 @@
 HOST_NUMBER = "0.0.0.0"
 PORT_NUMBER = 8000
-MODEL_PATH = r'pizza_price_prediction/artifacts/Pizza_Price_Prediction_Model.pkl'
+MODEL_PATH = 'artifacts/Pizza_Price_Prediction_Model.pkl'
