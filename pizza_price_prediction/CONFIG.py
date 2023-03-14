b90ad3cb5a491396fdf97c6f9dca1e364ceee645
Shridhar Shende <88563949+shri50@users.noreply.github.com>
2023-03-14 22:24:11 +0530

    commit

--- pizza_price_prediction/CONFIG.py
+++ pizza_price_prediction/CONFIG.py
@@ -1,3 +1,3 @@
 HOST_NUMBER = "0.0.0.0"
 PORT_NUMBER = 8000
-MODEL_PATH = '/pizza_price_prediction/artifacts/Pizza_Price_Prediction_Model.pkl'
+MODEL_PATH = '/home/ubuntu/Pizza_Price_Prediction/pizza_price_prediction/artifacts/Pizza_Price_Prediction_Model.pkl'
